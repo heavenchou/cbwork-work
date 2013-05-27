a0ed83a13549ee93fc08237bfd7f1a8dcb01ee81
heavenchou <heaven.chou@gmail.com>
2013-05-27 10:20:16 +0800

    2013/05/27 第一次上傳 git

---
+++ bin/config.pl
@@ -0,0 +1,9 @@
+# cbeta.plx 共用參數檔
+$log_file="/temp/checkxml.txt"; # 輸出記錄檔
+$xml_in_dir="/cbwork/xml"; # xml 輸入目錄
+#$xml_in_dir="z:/xml"; # xml 輸入目錄
+$xml_out_dir="x:/cbreader/xml"; # xml 輸出目錄
+$gaiji_cb_in_dir="c:/cbeta/cbreader/gaiji-cb"; # 缺字圖檔目錄
+$sd_gif_in_dir = "c:/cbeta/cbreader/sd-gif";
+$rj_gif_in_dir = "c:/cbeta/cbreader/rj-gif";
+$figure_in_dir = "c:/cbeta/cbreader/figures"; # 內文圖檔目錄
---
+++ cbent-all.bat
@@ -0,0 +1,355 @@
+
+cd C:\
+call xml
+cd C:\cbwork\xml
+
+goto start
+
+cd T01
+call cbent
+cd..
+cd T02
+call cbent
+cd..
+cd T03
+call cbent
+cd..
+cd T04
+call cbent
+cd..
+cd T05
+call cbent
+cd..
+cd T06
+call cbent
+cd..
+cd T07
+call cbent
+cd..
+cd T08
+call cbent
+cd..
+cd T09
+call cbent
+cd..
+cd T10
+call cbent
+cd..
+cd T11
+call cbent
+cd..
+cd T12
+call cbent
+cd..
+cd T13
+call cbent
+cd..
+cd T14
+call cbent
+cd..
+cd T15
+call cbent
+cd..
+cd T16
+call cbent
+cd..
+cd T17
+call cbent
+cd..
+cd T18
+call cbent
+cd..
+cd T19
+call cbent
+cd..
+cd T20
+call cbent
+cd..
+cd T21
+call cbent
+cd..
+cd T22
+call cbent
+cd..
+cd T23
+call cbent
+cd..
+cd T24
+call cbent
+cd..
+cd T25
+call cbent
+cd..
+cd T26
+call cbent
+cd..
+cd T27
+call cbent
+cd..
+cd T28
+call cbent
+cd..
+cd T29
+call cbent
+cd..
+cd T30
+call cbent
+cd..
+cd T31
+call cbent
+cd..
+cd T32
+call cbent
+cd..
+cd T33
+call cbent
+cd..
+cd T34
+call cbent
+cd..
+cd T35
+call cbent
+cd..
+cd T36
+call cbent
+cd..
+cd T37
+call cbent
+cd..
+cd T38
+call cbent
+cd..
+cd T39
+call cbent
+cd..
+cd T40
+call cbent
+cd..
+cd T41
+call cbent
+cd..
+cd T42
+call cbent
+cd..
+cd T43
+call cbent
+cd..
+cd T44
+call cbent
+cd..
+cd T45
+call cbent
+cd..
+cd T46
+call cbent
+cd..
+cd T47
+call cbent
+cd..
+cd T48
+call cbent
+cd..
+cd T49
+call cbent
+cd..
+cd T50
+call cbent
+cd..
+cd T51
+call cbent
+cd..
+cd T52
+call cbent
+cd..
+cd T53
+call cbent
+cd..
+cd T54
+call cbent
+cd..
+cd T55
+call cbent
+cd..
+cd T85
+call cbent
+
+
+cd..
+cd X17
+call cbent
+cd..
+cd X18
+call cbent
+cd..
+cd X19
+call cbent
+cd..
+cd X20
+call cbent
+cd..
+cd X21
+call cbent
+cd..
+cd X22
+call cbent
+cd..
+cd X23
+call cbent
+cd..
+cd X24
+call cbent
+cd..
+cd X25
+call cbent
+cd..
+cd X26
+call cbent
+cd..
+cd X27
+call cbent
+cd..
+cd X28
+call cbent
+cd..
+cd X29
+call cbent
+cd..
+cd X30
+call cbent
+cd..
+cd X31
+call cbent
+cd..
+cd X32
+call cbent
+cd..
+cd X33
+call cbent
+cd..
+cd X34
+call cbent
+cd..
+cd X35
+call cbent
+cd..
+cd X36
+call cbent
+cd..
+cd X37
+call cbent
+cd..
+cd X38
+call cbent
+cd..
+cd X39
+call cbent
+cd..
+cd X40
+call cbent
+cd..
+cd X41
+call cbent
+cd..
+cd X42
+call cbent
+cd..
+cd X43
+call cbent
+cd..
+cd X44
+call cbent
+cd..
+cd X45
+call cbent
+cd..
+cd X46
+call cbent
+cd..
+cd X47
+call cbent
+cd..
+cd X48
+call cbent
+cd..
+cd X49
+call cbent
+cd..
+cd X50
+call cbent
+cd..
+cd X51
+call cbent
+cd..
+cd X53
+call cbent
+
+goto end
+
+cd..
+cd X63
+call cbent
+cd..
+cd X64
+call cbent
+cd..
+cd X65
+call cbent
+cd..
+cd X66
+call cbent
+cd..
+cd X67
+call cbent
+cd..
+cd X68
+call cbent
+cd..
+cd X69
+call cbent
+cd..
+cd X70
+call cbent
+cd..
+cd X71
+call cbent
+cd..
+cd X72
+call cbent
+cd..
+cd X73
+call cbent
+cd..
+cd X78
+call cbent
+cd..
+cd X79
+call cbent
+cd..
+cd X80
+call cbent
+cd..
+cd X81
+call cbent
+cd..
+cd X82
+call cbent
+cd..
+cd X83
+call cbent
+cd..
+cd X84
+call cbent
+cd..
+cd X85
+call cbent
+cd..
+cd X86
+call cbent
+cd..
+cd X87
+call cbent
+cd..
+cd X88
+call cbent
+cd..
+:end
